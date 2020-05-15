78f53826ec471450d2d40e4bad21f894dcae7fd9
bgh1234554 <54991452+bgh1234554@users.noreply.github.com>
2020-05-15 09:54:31 +0900

    자료구조론 숙제 1
    Sparse Matrix의 합과 transpose를 해본다.

---
+++ ArraySparseMatrix.java
@@ -0,0 +1,122 @@
+package cse2010.homework1;
+
+public class ArraySparseMatrix implements SparseMatrix {
+
+    public static final int DEFAULT_CAPACITY = 1024;
+
+    private int rowCount;
+    private int columnCount;
+    private int elemCount;
+    private Entry[] elements = new Entry[0];
+
+    public ArraySparseMatrix(int rowCount, int columnCount, int capacity) {
+        elements = new Entry[capacity];
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
+        this.elemCount = 0;
+    }
+
+    public ArraySparseMatrix(int rowCount, int columnCount) {
+        this(rowCount, columnCount, DEFAULT_CAPACITY);
+    }
+
+    /*
+     * This routine simulates reading from files using two-dimensional matrix.
+     */
+    public static SparseMatrix create(double[][] aMatrix, int rowCount, int columnCount, int elemCount) {
+        ArraySparseMatrix matrix = new ArraySparseMatrix(rowCount, columnCount, elemCount);
+
+        int nonZeroCount = 0;
+        for (int i = 0; i < aMatrix.length; i++)
+            for (int j = 0; j < aMatrix[i].length; j++) {
+                if (Double.compare(aMatrix[i][j], 0.0) != 0) {
+                    matrix.put(new Entry(i, j, aMatrix[i][j]));
+                    nonZeroCount++;
+                }
+            }
+
+            /*
+             * your code goes here
+             */ 
+            // if (nonZeroCount != elemCount) throw IllegalStateException saying 
+            // "Non zero count doesn't match"
+        
+
+        return matrix;
+    }
+
+    private void put(Entry entry) {
+        elements[elemCount++] = entry;
+    }
+
+    @Override
+    public SparseMatrix transpose() {
+
+        /*
+         *   Your code goes here
+         */
+
+        return null;
+    }
+
+    @Override
+    public SparseMatrix add(SparseMatrix other) {
+        if (this.rowCount != other.getRowCount() || this.columnCount != other.getColumnCount())
+            throw new IllegalArgumentException("Matrix size doesn't match");
+
+        /*
+         *  Your code goes here
+         */
+
+        return null;
+    }
+
+    public Entry[] getElements() {
+        return elements;
+    }
+
+    @Override
+    public SparseMatrix multiply(SparseMatrix aMatrix) {
+        throw new IllegalStateException("Not implemented");
+    }
+
+    @Override
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    @Override
+    public int getElemCount() {
+        return elemCount;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof ArraySparseMatrix)) return false;
+        ArraySparseMatrix other = (ArraySparseMatrix) obj;
+
+        if (rowCount != other.rowCount || columnCount != other.columnCount || elemCount != other.elemCount)
+            return false;
+
+        for (int i = 0; i < elements.length; i++) {
+            if (!elements[i].equals(other.elements[i])) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(rowCount + "\t" + columnCount + "\t" + elemCount + "\n");
+        for (int i = 0; i < elemCount; i ++)
+            builder.append(elements[i] + "\n");
+
+        return builder.toString();
+    }
+}
